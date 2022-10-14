8d4c4d2417d60fb31791796512af4ff9fb031493
garre <garrettprice27@gmail.com>
2022-10-14 16:36:31 -0600

    Completed fibonacci function

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -39,15 +39,18 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        int array[];
-        array = new int[n];
+        if(n == 0){
+            return 0;
+        }
+        int[] array;
+        array = new int[n+1];
         array[0] = 0;
         array[1] = 1;
 
-        for(int i = 2; i < n; i++){
+        for(int i = 2; i <= n; i++){
             array[i] = array[i-1] + array[i-2];
         }
-        return array[n-1];
+        return array[n];
     }
 
 
